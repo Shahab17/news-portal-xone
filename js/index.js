f2854f144eba45c9d28645fe941e6c44dac6e395
ShahabMat <cse_1812020090@lus.ac.bd>
2023-09-03 18:41:54 +0600

    dynamically added a categories names

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
-console.log('hey im here to connect w u')
+
+
+const newsAllCategoriesData = async () =>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/news/categories`);
+    const data = await res.json();
+    const allCategoriesData = data.data.news_category.slice(0,4);
+    console.log(allCategoriesData)
+
+
+    const getCategoriesNames = document.getElementById('tab-container');
+    
+    allCategoriesData.forEach(category => {
+        console.log(category.category_name)
+
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <a class="tab text-2xl ">${category.category_name}</a>
+        `;
+
+        getCategoriesNames.appendChild(div)
+    });
+   
+
+}
+
+
+
+newsAllCategoriesData();
